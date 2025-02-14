300558980071627acb98fb3faccefe035f8db5c5
deephar-kaur <deepharkaur62@gmail.com>
2025-02-14 13:23:58 -0500

    add welcome and thank you component

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// src/App.jsx
+import "./styles.css";
+import Welcome from "./Welcome";
+import ThankYou from "./ThankYou";
+
+function App() {
+  return (
+    <div className="container">
+      <Welcome />
+      <ThankYou />
+    </div>
+  );
+}
+
+export default App;
